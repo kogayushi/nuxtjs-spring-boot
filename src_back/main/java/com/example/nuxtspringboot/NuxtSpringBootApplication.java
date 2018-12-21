42a1ff6463fc8aabf1ee31b603d12e4379dd836d
yushi koga <youshe.old.pleasure@gmail.com>
2018-12-21 10:45:02 +0900

    initial commit

---
+++ src_back/main/java/com/example/nuxtspringboot/NuxtSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.nuxtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuxtSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuxtSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ src_back/main/java/com/example/nuxtspringboot/SampleResource.java
@@ -0,0 +1,16 @@
+package com.example.nuxtspringboot;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class SampleResource {
+
+  @GetMapping(path = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String hello() {
+    return "hello";
+  }
+}
