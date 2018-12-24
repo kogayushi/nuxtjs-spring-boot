43bf7156b76d677771921381cb5a5c4e959e7431
yushi koga <youshe.old.pleasure@gmail.com>
2018-12-24 15:39:08 +0900

    add comment for explanation

--- src_back/main/java/com/example/nuxtspringboot/Html5HistoryModeResourceConfig.java
+++ src_back/main/java/com/example/nuxtspringboot/Html5HistoryModeResourceConfig.java
@@ -18,17 +18,17 @@ public class Html5HistoryModeResourceConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/**")
-            .addResourceLocations(resourceProperties.getStaticLocations())
-            .resourceChain(false)
-            .addResolver(new SpaPageResourceResolver());
+    registry.addResourceHandler("/**") // 全パスをこのリソースハンドラーの処理対象にする
+            .addResourceLocations(resourceProperties.getStaticLocations()) // 静的リソース配置先のパスを指定する
+            .resourceChain(resourceProperties.getChain().isCache()) // 開発時はfalse、本番はtrueが望ましい。trueにしておくとメモリ上にキャッシュされるためI/Oが軽減される
+            .addResolver(new SpaPageResourceResolver()); // 拡張したPathResourceResolverを読み込ませる
   }
 
   public static class SpaPageResourceResolver extends PathResourceResolver {
     @Override
     protected Resource getResource(String resourcePath, Resource location) throws IOException {
-      Resource resource = super.getResource(resourcePath, location);
-      return resource != null ? resource : super.getResource("index.html", location);
+      Resource resource = super.getResource(resourcePath, location); // まずはPathResourceResolverで静的リソースを取得する
+      return resource != null ? resource : super.getResource("index.html", location); // 取得できなかった場合は、index.htmlを返す
     }
   }
 }
--- src_back/main/java/com/example/nuxtspringboot/SpringSecurityConfiguration.java
+++ src_back/main/java/com/example/nuxtspringboot/SpringSecurityConfiguration.java
@@ -21,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    // loginとlogoutを設定する。基本的な設定なので詳細は割愛
     http
       .authorizeRequests()
       .anyRequest().authenticated()
@@ -32,24 +33,26 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
       .permitAll();
 
     http.exceptionHandling()
+        // '/api/**'へ未認証状態でのアクセスには401を返す
         .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), new AntPathRequestMatcher("/api/**"))
+        // 上記パス以外への未認証状態へのアクセスは302リダイレクトで'/login'へ遷移させる
         .defaultAuthenticationEntryPointFor(new LoginUrlAuthenticationEntryPoint("/login"), AnyRequestMatcher.INSTANCE);
 
-    CookieCsrfTokenRepository cookieCsrfTokenRepository = new CookieCsrfTokenRepository();
-    cookieCsrfTokenRepository.setCookieHttpOnly(false);
+    CookieCsrfTokenRepository cookieCsrfTokenRepository = new CookieCsrfTokenRepository(); // ajaxでcsrf tokenを利用するのでcookieに出力する
+    cookieCsrfTokenRepository.setCookieHttpOnly(false); // ajaxでも利用するため、httpOnlyはfalseにしておく
     http.csrf().csrfTokenRepository(cookieCsrfTokenRepository);
 
+    // RESTful APIを公開する場合、攻撃されやすくなるのでcorsの設定をしておく
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
     corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
-    corsConfiguration.addAllowedOrigin("http://localhost:3000");
-
+    corsConfiguration.addAllowedOrigin("http://localhost:3000"); // 実際は環境ごとにドメインが変わるはずなので、設定で動的に変更でき料にする
     UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
-    corsSource.registerCorsConfiguration("/**", corsConfiguration);
-
+    corsSource.registerCorsConfiguration("/**", corsConfiguration); // すべてのパスを対象にする
     http.cors().configurationSource(corsSource);
   }
 
+  // デモ用設定
   @Bean
   @Override
   public UserDetailsService userDetailsService() {
