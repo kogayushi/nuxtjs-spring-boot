e5373672cf755ebc5f5e92b03ea89c73dede3495
yushi koga <youshe.old.pleasure@gmail.com>
2018-12-29 16:50:45 +0900

    add comment for blog

--- src_front/pages/login/index.vue
+++ src_front/pages/login/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>redirecting...</div>
+  <div>redirecting...</div> <!-- 一瞬表示されてしまうので、何かダミーを表示しておく -->
 </template>
 
 <script>
 export default {
-  mounted() {
+  beforeCreate() {
+    // 表示された瞬間、バックエンドのログインページにリダイレクトさせる
+    // 本番運用時は`/login`とログインページは一致しているのでこのコンポーネントは動かない
     window.location = 'http://localhost:8080/login'
   }
 }
