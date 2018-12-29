862900442123d8a295d13786cde29789d208521b
yushi koga <youshe.old.pleasure@gmail.com>
2018-12-29 23:06:44 +0900

    logout on load

--- src_front/components/Logout.vue
+++ src_front/components/Logout.vue
@@ -3,6 +3,7 @@
     :action="url"
     method="post">
     <input
+      ref="button"
       class="button--grey"
       type="submit"
       value="Sing Out">
@@ -17,9 +18,15 @@
 export default {
   name: 'Logout',
   props: {
+    // ログアウトURLをpropsで受け取る
     url: {
       type: String,
       required: true
+    },
+    // 自動submit有無のフラグ
+    logoutOnLoad: {
+      type: Boolean,
+      default: false
     }
   },
   data: function() {
@@ -27,12 +34,21 @@ export default {
       xsrfToken: ''
     }
   },
-  created: function() {
+  created() {
+    // ログアウトページにPOSTするために、cookieからcsrf tokenを取得する
     this.xsrfToken = ((document.cookie + ';').match('XSRF-TOKEN=([^¥S;]*)') ||
       [])[1]
+
+    // 開発時用機能、コンポーネントが作られたときにログアウトボタンを自動submitさせる
+    if (this.logoutOnLoad) {
+      // コンポーネントが描画されてないとsubmitできないので、描画を待つ
+      this.$nextTick(function() {
+        // refを利用してボタンを参照
+        let button = this.$refs.button
+        // submitする
+        button.click()
+      })
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>
--- src_front/pages/logout/index.vue
+++ src_front/pages/logout/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <logout url="http://localhost:8080/logout"/>
+  <div>
+    <p>progressing</p>
+    <logout
+      v-show="false"
+      :logout-on-load="true"
+      url="http://localhost:8080/logout"/>
+  </div>
 </template>
 
 <script>
 import Logout from '~/components/Logout.vue'
+
 export default {
   components: {
     Logout
