55ede2e735553cbc5121dd32b5cb4b43f57ce0ee
yushi koga <youshe.old.pleasure@gmail.com>
2019-07-12 19:01:08 +0900

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,13 +56,19 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      }
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
+          options:{
+            fix:true
+          }
         })
       }
     }
